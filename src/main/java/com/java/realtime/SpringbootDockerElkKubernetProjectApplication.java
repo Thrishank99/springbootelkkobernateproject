76b831cb64aa14c6b6b70ebe7c235c9466a94ea7
Thrishank99 <thrishank966@gmail.com>
2024-07-23 16:36:29 +0530

    syssout added

--- src/main/java/com/java/realtime/SpringbootDockerElkKubernetProjectApplication.java
+++ src/main/java/com/java/realtime/SpringbootDockerElkKubernetProjectApplication.java
@@ -17,6 +17,7 @@ public class SpringbootDockerElkKubernetProjectApplication extends SpringBootSer
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerElkKubernetProjectApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
